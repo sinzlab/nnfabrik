213ad2dfc45bf410aa879db2c14cdc5b5bb5a2fe
KonstantinWilleke <konstantin.willeke@gmail.com>
2019-09-19 18:16:13 +0200

    Adds dataloader as function input

--- models/V1_models.py
+++ models/V1_models.py
@@ -1,14 +1,15 @@
-
 import numpy as np
-
 import torch
-
 from mlutils.layers.readouts import PointPooled2d
 from mlutils.layers.cores import Stacked2dCore
 from torch import nn
 
-
-def Core2D_PointRO(input_shape, output_shape, seed, param1=1):
+def stacked2d_core_point_readout(dataloader, seed,
+                   input_channels=1, hidden_channels=32, input_kern=15,
+                   hidden_kern=7, layers=3, gamma_hidden=0, gamma_input=0.1,
+                   skip=0, final_nonlinearity=True, core_bias=False, momentum=0.7,
+                   pad_input=True, batch_norm=True, hidden_dilation=1,
+                   pool_steps=2, pool_kern=7, readout_bias=True, init_range=0.1):
 
     class Encoder(nn.Module):
 
@@ -30,48 +31,52 @@ def Core2D_PointRO(input_shape, output_shape, seed, param1=1):
             x = self.core(x)
             x = self.readout(x)
 
-    core = Stacked2dCore(input_channels=1,
-                         hidden_channels=32,
-                         input_kern=15,
-                         hidden_kern=7,
-                         layers=3,
-                         gamma_hidden=0,
-                         gamma_input=0.1,
-                         skip=0,
-                         final_nonlinearity=True,
-                         bias=False,
-                         momentum=0.75,
-                         pad_input=True,
-                         batch_norm=True,
-                         hidden_dilation=1)
+    # get a stacked2D core from mlutils
+    core = Stacked2dCore(input_channels=input_channels,
+                         hidden_channels=hidden_channels,
+                         input_kern=input_kern,
+                         hidden_kern=hidden_kern,
+                         layers=layers,
+                         gamma_hidden=gamma_hidden,
+                         gamma_input=gamma_input,
+                         skip=skip,
+                         final_nonlinearity=final_nonlinearity,
+                         bias=core_bias,
+                         momentum=momentum,
+                         pad_input=pad_input,
+                         batch_norm=batch_norm,
+                         hidden_dilation=hidden_dilation)
 
-
+    input_shape, output_shape = get_in_out_dimensions(dataloader)
     readout_in_shape = Encoder.get_readout_in_shape(core,input_shape)
-    print(readout_in_shape)
     num_neurons=output_shape[1]
-    print("number of neurons")
-    print(num_neurons)
 
+    # get a PointPooled Readout from mlutils
     readout = PointPooled2d(readout_in_shape,
                             num_neurons,
-                            pool_steps=2,
-                            pool_kern=7,
-                            bias=True,
-                            init_range=0.1)
-
-
-    # Readout
+                            pool_steps=pool_steps,
+                            pool_kern=pool_kern,
+                            bias=readout_bias,
+                            init_range=init_range)
     gamma_readout = 0.5
     def regularizer():
         return readout.feature_l1() * gamma_readout
     readout.regularizer = regularizer
 
+    # pass dataloader to modelbuilder, so th
+    #
+    train_loader = dataloader["train_loader"]
+    train_responses = []
+    for *, responses, * in train_loader:
+        train_responses.append(responses)
+    model.readout.bias.data = torch.as_tensor(train_responses).to(torch.float).mean((0))
+
     np.random.seed(seed)
     model = Encoder(core, readout)
-    # initialize the readout
-    model.readout.bias.data = torch.as_tensor(responses).to(torch.float).mean((0))
-    # initialize the core
     model.core.initialize()
     model = model.cuda()
-
     return model
+
+
+
+
