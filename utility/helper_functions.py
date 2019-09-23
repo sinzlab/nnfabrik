b2833c5c2cbc3fae86d1036c66cc2d5e26bec384
KonstantinWilleke <konstantin.willeke@gmail.com>
2019-09-23 11:24:18 +0200

    initial commit
    adds the first helper function to the utils directory. ultimately they should be merged into the mlutils directory

---
+++ utility/helper_functions.py
@@ -0,0 +1,37 @@
+def get_in_out_dimensions(self, data_loader):
+    """
+        gets the input and output dimensions from the dataloader.
+
+    :param
+        dataloader: is expected to be a dict in the form of
+                        {
+                        'train_loader': torch.utils.data.DataLoader,
+                         'val_loader': torch.utils.data.DataLoader,
+                         'test_loader: torch.utils.data.DataLoader,
+                         }
+
+            each loader should have as first argument the input in the form of
+                [batch_size, channels, px_x, px_y, ...]
+
+            each loader should have as second argument the out in some form
+                [batch_size, output_units, ...]
+
+
+    :return:
+        input_dim: input dimensions, expected to be a tuple in the form of input.shape.
+                    for example: (batch_size, channels, px_x, px_y, ...)
+        output_dim: out dimensions, expected to be a tuple in the form of output.shape.
+                    for example: (batch_size, output_units, ...)
+
+    """
+    train_loader = data_loader["train_loader"]
+    input_batch, output_batch, _ = next(iter(train_loader))
+    return input_batch.shape, output_batch.shape
+
+def get_core_output_shape(core, input_shape):
+    train_state = core.training
+    core.eval()
+    input_tensor = torch.ones(input_shape)
+    tensor_out = core(input_tensor).shape[1:]
+    core.train(train_state)
+    return tensor_out
