9e5d9edc33ad448a641665cfff469e6a7f3be92b
KonstantinWilleke <konstantin.willeke@gmail.com>
2019-09-24 16:45:26 +0200

    Adds readout bias initialization
    Also adds pytorch random seed initialization

--- models/V1_models.py
+++ models/V1_models.py
@@ -2,7 +2,10 @@ import numpy as np
 import torch
 from mlutils.layers.readouts import PointPooled2d
 from mlutils.layers.cores import Stacked2dCore
-from torch import nn
+from torch import nn as nn
+from utility.helper_functions import *
+from torch.nn import functional as F
+
 
 def stacked2d_core_point_readout(dataloader, seed,
                    input_channels=1, hidden_channels=32, input_kern=15,
@@ -18,18 +21,13 @@ def stacked2d_core_point_readout(dataloader, seed,
             self.core = core
             self.readout = readout
 
-        @staticmethod
-        def get_readout_in_shape(core, input_shape):
-            train_state = core.training
-            core.eval()
-            input_tensor = torch.ones(input_shape)
-            tensor_out = core(input_tensor).shape[1:]
-            core.train(train_state)
-            return tensor_out
-
         def forward(self, x):
             x = self.core(x)
             x = self.readout(x)
+            return F.elu(x) + 1
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
     # get a stacked2D core from mlutils
     core = Stacked2dCore(input_channels=input_channels,
@@ -48,7 +46,7 @@ def stacked2d_core_point_readout(dataloader, seed,
                          hidden_dilation=hidden_dilation)
 
     input_shape, output_shape = get_in_out_dimensions(dataloader)
-    readout_in_shape = Encoder.get_readout_in_shape(core,input_shape)
+    readout_in_shape = get_core_output_shape(core,input_shape)
     num_neurons=output_shape[1]
 
     # get a PointPooled Readout from mlutils
@@ -63,20 +61,24 @@ def stacked2d_core_point_readout(dataloader, seed,
         return readout.feature_l1() * gamma_readout
     readout.regularizer = regularizer
 
-    # pass dataloader to modelbuilder, so th
-    #
-    train_loader = dataloader["train_loader"]
-    train_responses = []
-    for *, responses, * in train_loader:
-        train_responses.append(responses)
-    model.readout.bias.data = torch.as_tensor(train_responses).to(torch.float).mean((0))
 
-    np.random.seed(seed)
+
+    train_loader = dataloader["train_loader"]
+
+    n_batches = 25
+    train_responses = np.zeros((n_batches, num_neurons))
+    train_valid_responses = np.zeros((n_batches, num_neurons))
+    for i in range(n_batches):
+        _, responses, valid_responses = next(iter(train_loader))
+        train_responses[i, :] = responses.mean((0)).cpu().numpy()
+        train_valid_responses[i, :] = valid_responses.mean((0)).cpu().numpy()
+
+    avg_responses = np.mean(train_responses / train_valid_responses, axis=0)
+
     model = Encoder(core, readout)
     model.core.initialize()
-    model = model.cuda()
+    model.readout.bias.data = torch.as_tensor(avg_responses).to(torch.float).mean((0))
     return model
-
 
 
 
