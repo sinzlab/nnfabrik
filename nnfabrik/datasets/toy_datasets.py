576ed739b2fbeb0976e6c7e5642ba3888357ed13
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2019-11-06 10:08:42 +0100

    Add example for using nnFabrik for other projects

---
+++ nnfabrik/datasets/toy_datasets.py
@@ -0,0 +1,6 @@
+def toy_dataset(seed, param1=1):
+    """
+    returns a simple dict with 3 loaders
+    """
+    data_loader = dict(train_loader=1, val_loader=2, test_loader=3)
+    return data_loader
---
+++ nnfabrik/models/toy_models.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+    
+class ToyModel(nn.Module):
+    def __init__(self, kern=16, kern2=32):
+        super().__init__()
+        self.kern = kern
+        self.lin1 = nn.Linear(128, kern)
+        self.lin2 = nn.Linear(kern, 16)
+        
+    def forward(self, x):
+        return self.lin2(self.lin1(x))
+    
+def toy_model(input_dim, seed, kern=16, kern2=32):
+    return ToyModel(kern=kern, kern2=kern2)
