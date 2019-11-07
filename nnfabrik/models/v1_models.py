06e19f936945c286e3851416e8e5abb48f452d48
KonstantinWilleke <konstantin.willeke@gmail.com>
2019-11-07 21:18:29 +0100

    Changes input arguments
    Input channels are now read from the dataloader. Imports are updated - superfluous imports are deleted.

--- nnfabrik/models/v1_models.py
+++ nnfabrik/models/v1_models.py
@@ -1,18 +1,18 @@
-import numpy as np
-import torch
 from mlutils.layers.readouts import PointPooled2d
 from mlutils.layers.cores import Stacked2dCore
 from torch import nn as nn
-from ..utility.nn_helpers import get_io_dims, get_module_output, set_random_seed
+from utility.nn_helpers import get_io_dims, get_module_output, set_random_seed
 from torch.nn import functional as F
 
 
-def stacked2d_core_point_readout(dataloader, seed,
-                   input_channels=1, hidden_channels=32, input_kern=15,
-                   hidden_kern=7, layers=3, gamma_hidden=0, gamma_input=0.1,
-                   skip=0, final_nonlinearity=True, core_bias=False, momentum=0.7,
-                   pad_input=True, batch_norm=True, hidden_dilation=1,
-                   pool_steps=2, pool_kern=7, readout_bias=True, init_range=0.1):
+def stacked2d_core_point_readout(dataloader, seed, hidden_channels=32, input_kern=15,
+                                 hidden_kern=7, layers=3, gamma_hidden=0, gamma_input=0.1,
+                                 skip=0, final_nonlinearity=True, core_bias=False, momentum=0.9,
+                                 pad_input=True, batch_norm=True, hidden_dilation=1,
+                                 pool_steps=2, pool_kern=7, readout_bias=True, init_range=0.1,
+                                 gamma_readout=0.5, laplace_padding=None):
+    input_shape, output_shape = get_io_dims(dataloader["train_loader"])
+    input_channels = input_shape[1]
 
     class Encoder(nn.Module):
 
@@ -22,9 +22,9 @@ def stacked2d_core_point_readout(dataloader, seed,
             self.readout = readout
 
         def forward(self, x):
-            x = self.core(x)
-            x = self.readout(x)
-            return F.elu(x) + 1
+            output_of_the_core = self.core(x)
+            readout_output = self.readout(output_of_the_core)
+            return F.elu(readout_output) + 1
 
     set_random_seed(seed)
 
@@ -42,22 +42,24 @@ def stacked2d_core_point_readout(dataloader, seed,
                          momentum=momentum,
                          pad_input=pad_input,
                          batch_norm=batch_norm,
-                         hidden_dilation=hidden_dilation)
+                         hidden_dilation=hidden_dilation,
+                         laplace_padding=laplace_padding)
 
-    input_shape, output_shape = get_io_dims(dataloader["train_loader"])
-    readout_in_shape = get_module_output(core,input_shape)
-    num_neurons=output_shape[1]
-
+    readout_in_shape = get_module_output(core, input_shape)
+    num_neurons = output_shape[1]
     # get a PointPooled Readout from mlutils
     readout = PointPooled2d(readout_in_shape[1:],
                             num_neurons,
                             pool_steps=pool_steps,
-                            pool_kern=pool_kern,
+                            pool_kern=int(pool_kern),
                             bias=readout_bias,
                             init_range=init_range)
-    gamma_readout = 0.5
+
+    gamma_readout = gamma_readout = 0.5
+
     def regularizer():
         return readout.feature_l1() * gamma_readout
+
     readout.regularizer = regularizer
 
     _, train_responses, weights = dataloader["train_loader"].dataset[:]
@@ -68,6 +70,4 @@ def stacked2d_core_point_readout(dataloader, seed,
     model.core.initialize()
     model.readout.bias.data = avg_responses
     return model
-
-
 
