3ae5f58c28e94665dc3794e3068d772824f011f8
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2019-12-11 12:51:00 +0100

    Make column names consistent in the tables.

--- nnfabrik/main.py
+++ nnfabrik/main.py
@@ -33,7 +33,7 @@ schema = dj.schema(dj.config.get('schema_name', 'nnfabrik_core'))
 @schema
 class Fabrikant(dj.Manual):
     definition = """
-    architect_name: varchar(32)       # Name of the contributor that added this entry
+    fabrikant_name: varchar(32)       # Name of the contributor that added this entry
     ---
     email: varchar(64)      # e-mail address
     affiliation: varchar(32) # conributor's affiliation
@@ -43,103 +43,104 @@ class Fabrikant(dj.Manual):
     @classmethod
     def get_current_user(cls):
         """
-        Lookup the architect_name in Fabrikant corresponding to the currently logged in DataJoint user
-        Returns: architect_name if match found, else None
+        Lookup the fabrikant_name in Fabrikant corresponding to the currently logged in DataJoint user
+        Returns: fabrikant_name if match found, else None
         """
         username = cls.connection.get_user().split('@')[0]
         entry = (Fabrikant & dict(dj_username=username))
         if entry:
-            return entry.fetch1('architect_name')
+            return entry.fetch1('fabrikant_name')
 
 
 
 @schema
 class Model(dj.Manual):
     definition = """
-    configurator: varchar(64)   # name of the configuration function
-    config_hash: varchar(64)    # hash of the configuration object
+    model_fn:                   varchar(64)   # name of the model function
+    model_hash:                 varchar(64)   # hash of the model configuration
     ---
-    config_object: longblob     # configuration object to be passed into the function
-    -> Fabrikant.proj(model_architect='architect_name')
-    model_comment='' : varchar(64)  # short description
-    model_ts=CURRENT_TIMESTAMP: timestamp    # UTZ timestamp at time of insertion
+    model_config:               longblob      # model configuration to be passed into the function
+    -> Fabrikant.proj(model_fabrikant='fabrikant_name')
+    model_comment='' :          varchar(64)   # short description
+    model_ts=CURRENT_TIMESTAMP: timestamp     # UTZ timestamp at time of insertion
     """
 
     @property
     def fn_config(self):
-        model_fn, model_config = self.fetch1('configurator', 'config_object')
+        model_fn, model_config = self.fetch1('model_fn', 'model_config')
         model_config = cleanup_numpy_scalar(model_config)
         return model_fn, model_config
 
-    def add_entry(self, configurator, config_object, model_architect=None, model_comment=''):
+    def add_entry(self, model_fn, model_config, model_fabrikant=None, model_comment=''):
         """
-        configurator -- name of the function/class that's callable
-        config_object -- actual Python object
+        model_fn -- name of the function/class that's callable
+        model_config -- actual Python object
         """
-        module_path, class_name = split_module_name(configurator)
-        config_fn = dynamic_import(module_path, class_name) if module_path else eval('models.' + configurator)
+        module_path, class_name = split_module_name(model_fn)
+        model_fn_obj = dynamic_import(module_path, class_name) if module_path else eval('models.' + model_fn)
         try:
-            callable(config_fn)
+            callable(model_fn_obj)
         except NameError:
-            warnings.warn("configurator function does not exist. Table entry rejected")
+            warnings.warn("Model function does not exist. Table entry rejected")
             return
 
-        config_hash = make_hash(config_object)
-        if model_architect is None:
-            model_architect = Fabrikant.get_current_user()
-        key = dict(configurator=configurator, config_hash=config_hash, config_object=config_object,
-                   model_architect=model_architect, model_comment=model_comment)
+        model_hash = make_hash(model_config)
+        if model_fabrikant is None:
+            model_fabrikant = Fabrikant.get_current_user()
+        key = dict(model_fn=model_fn, model_hash=model_hash, model_config=model_config,
+                   model_fabrikant=model_fabrikant, model_comment=model_comment)
         self.insert1(key)
 
     def build_model(self, dataloader, seed=None, key=None):
         print('Loading model...')
         if key is None:
             key = {}
-        configurator, config_object = (self & key).fn_config
+        model_fn, model_config = (self & key).fn_config
 
-        return get_model(configurator, config_object, dataloader, seed=seed)
+        return get_model(model_fn, model_config, dataloader, seed=seed)
 
 
 
 @schema
 class Dataset(dj.Manual):
     definition = """
-    dataset_loader: varchar(64)         # name of the dataset loader function
-    dataset_config_hash: varchar(64)    # hash of the configuration object
+    dataset_fn:                     varchar(64)    # name of the dataset loader function
+    dataset_hash:                   varchar(64)    # hash of the configuration object
     ---
-    dataset_config: longblob     # dataset configuration object
-    -> Fabrikant.proj(dataset_architect='architect_name')
-    dataset_comment='' : varchar(64)  # short description
-    dataset_ts=CURRENT_TIMESTAMP: timestamp    # UTZ timestamp at time of insertion
+    dataset_config:                 longblob       # dataset configuration object
+    -> Fabrikant.proj(dataset_fabrikant='fabrikant_name')
+    dataset_comment='' :            varchar(64)    # short description
+    dataset_ts=CURRENT_TIMESTAMP:   timestamp      # UTZ timestamp at time of insertion
     """
 
     @property
     def fn_config(self):
-        dataset_loader, dataset_config = self.fetch1('dataset_loader', 'dataset_config')
+        dataset_fn, dataset_config = self.fetch1('dataset_fn', 'dataset_config')
         dataset_config = cleanup_numpy_scalar(dataset_config)
-        return dataset_loader, dataset_config
+        return dataset_fn, dataset_config
 
-    def add_entry(self, dataset_loader, dataset_config, dataset_architect=None, dataset_comment=''):
+    def add_entry(self, dataset_fn, dataset_config, dataset_fabrikant=None, dataset_comment=''):
         """
         inserts one new entry into the Dataset Table
-        dataset_loader -- name of dataset function/class that's callable
+        dataset_fn -- name of dataset function/class that's callable
         dataset_config -- actual Python object with which the dataset function is called
         """
 
-        module_path, class_name = split_module_name(dataset_loader)
-        dataset_fn = dynamic_import(module_path, class_name) if module_path else eval('datasets.' + dataset_loader)
+        module_path, class_name = split_module_name(dataset_fn)
+        dataset_fn_obj = dynamic_import(module_path, class_name) if module_path else eval('datasets.' + dataset_fn)
+
         try:
-            callable(dataset_fn)
+            callable(dataset_fn_obj)
         except NameError:
-            warnings.warn("dataset_loader function does not exist. Table entry rejected")
+            warnings.warn("dataset function does not exist. Table entry rejected")
             return
 
-        if dataset_architect is None:
-            dataset_architect = Fabrikant.get_current_user()
+        if dataset_fabrikant is None:
+            dataset_fabrikant = Fabrikant.get_current_user()
 
-        dataset_config_hash = make_hash(dataset_config)
-        key = dict(dataset_loader=dataset_loader, dataset_config_hash=dataset_config_hash,
-                   dataset_config=dataset_config, dataset_architect=dataset_architect, dataset_comment=dataset_comment)
+        dataset_hash = make_hash(dataset_config)
+        key = dict(dataset_fn=dataset_fn, dataset_hash=dataset_hash,
+                   dataset_config=dataset_config, dataset_fabrikant=dataset_fabrikant, dataset_comment=dataset_comment)
         self.insert1(key)
 
     def get_dataloader(self, seed=None, key=None):
@@ -147,9 +148,9 @@ class Dataset(dj.Manual):
         Returns a dataloader for a given dataset loader function and its corresponding configurations
         dataloader: is expected to be a dict in the form of
                             {
-                            'train_loader': torch.utils.data.DataLoader,
-                             'val_loader': torch.utils.data.DataLoader,
-                             'test_loader: torch.utils.data.DataLoader,
+                            'train': torch.utils.data.DataLoader,
+                            'val': torch.utils.data.DataLoader,
+                            'test: torch.utils.data.DataLoader,
                              }
                              or a similar iterable object
                 each loader should have as first argument the input such that
@@ -160,71 +161,71 @@ class Dataset(dj.Manual):
         if key is None:
             key = {}
 
-        dataset_loader, dataset_config = (self & key).fn_config
+        dataset_fn, dataset_config = (self & key).fn_config
 
         if seed is not None:
             dataset_config['seed'] = seed # override the seed if passed in
 
-        return get_data(dataset_loader, dataset_config)
+        return get_data(dataset_fn, dataset_config)
 
 
 @schema
 class Trainer(dj.Manual):
     definition = """
-    training_function: varchar(64)     # name of the Trainer loader function
-    training_config_hash: varchar(64)  # hash of the configuration object
+    trainer_fn:                     varchar(64)     # name of the Trainer loader function
+    trainer_hash:                   varchar(64)     # hash of the configuration object
     ---
-    training_config: longblob          # training configuration object
-    -> Fabrikant.proj(trainer_architect='architect_name')
-    trainer_comment='' : varchar(64)  # short description
-    trainer_ts=CURRENT_TIMESTAMP: timestamp    # UTZ timestamp at time of insertion
+    trainer_config:                 longblob        # training configuration object
+    -> Fabrikant.proj(trainer_fabrikant='fabrikant_name')
+    trainer_comment='' :            varchar(64)     # short description
+    trainer_ts=CURRENT_TIMESTAMP:   timestamp       # UTZ timestamp at time of insertion
     """
 
     @property
     def fn_config(self):
-        training_function, training_config = self.fetch1('training_function', 'training_config')
-        training_config = cleanup_numpy_scalar(training_config)
-        return training_function, training_config
+        trainer_fn, trainer_config = self.fetch1('trainer_fn', 'trainer_config')
+        trainer_config = cleanup_numpy_scalar(trainer_config)
+        return trainer_fn, trainer_config
 
-    def add_entry(self, training_function, training_config, trainer_architect=None, trainer_comment=''):
+    def add_entry(self, trainer_fn, trainer_config, trainer_fabrikant=None, trainer_comment=''):
         """
         inserts one new entry into the Trainer Table
-        training_function -- name of trainer function/class that's callable
-        training_config -- actual Python object with which the trainer function is called
+        trainer_fn -- name of trainer function/class that's callable
+        trainer_config -- actual Python object with which the trainer function is called
         """
 
-        module_path, class_name = split_module_name(training_function)
-        trainer_fn = dynamic_import(module_path, class_name) if module_path else eval('training.' + training_function)
+        module_path, class_name = split_module_name(trainer_fn)
+        trainer_fn_obj = dynamic_import(module_path, class_name) if module_path else eval('training.' + trainer_fn)
         try:
-            callable(trainer_fn)
+            callable(trainer_fn_obj)
         except NameError:
-            warnings.warn("dataset_loader function does not exist. Table entry rejected")
+            warnings.warn("dataset function does not exist. Table entry rejected")
             return
 
-        training_config_hash = make_hash(training_config)
+        trainer_hash = make_hash(trainer_config)
 
-        if trainer_architect is None:
-            trainer_architect = Fabrikant.get_current_user()
+        if trainer_fabrikant is None:
+            trainer_fabrikant = Fabrikant.get_current_user()
 
-        key = dict(training_function=training_function, training_config_hash=training_config_hash,
-                   training_config=training_config, trainer_architect=trainer_architect,
+        key = dict(trainer_fn=trainer_fn, trainer_hash=trainer_hash,
+                   trainer_config=trainer_config, trainer_fabrikant=trainer_fabrikant,
                    trainer_comment=trainer_comment)
         self.insert1(key)
 
     def get_trainer(self, key=None, build_partial=True):
         """
-        Returns the training function for a given training function and its corresponding configurations
+        Returns the trainer function and its corresponding configurations
         """
         if key is None:
             key = {}
-        training_function, training_config = (self & key).fn_config
+        trainer_fn, trainer_config = (self & key).fn_config
 
         if build_partial:
             # build the configuration into the function
-            return get_trainer(training_function, training_config)
+            return get_trainer(trainer_fn, trainer_config)
         else:
             # return them separately
-            return get_trainer(training_function), training_config
+            return get_trainer(trainer_fn), trainer_config
 
 
 @schema
@@ -243,8 +244,8 @@ class TrainedModel(dj.Computed):
     -> Trainer
     -> Seed
     ---
-    score:   float  # loss
-    output: longblob  # trainer object's output
+    score:                             float        # loss
+    output:                            longblob     # trainer object's output
     ->[nullable] Fabrikant
     trainedmodel_ts=CURRENT_TIMESTAMP: timestamp    # UTZ timestamp at time of insertion
     """
@@ -302,8 +303,8 @@ class TrainedModel(dj.Computed):
 
         else:
 
-            # by default try to lookup the architect corresponding to the current DJ user
-            architect_name = Fabrikant.get_current_user()
+            # by default try to lookup the fabrikant corresponding to the current DJ user
+            fabrikant_name = Fabrikant.get_current_user()
             seed = (Seed & key).fetch1('seed')
 
             config_dict = self.get_full_config(key)
@@ -319,7 +320,7 @@ class TrainedModel(dj.Computed):
 
                 key['score'] = score
                 key['output'] = output
-                key['architect_name'] = architect_name
+                key['fabrikant_name'] = fabrikant_name
                 self.insert1(key)
 
                 key['model_state'] = filepath
