510e661cbdbeb64817fedd7cc5685d532a218248
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2019-12-17 14:58:06 +0100

    Change the name of the dataset function. Replace device with cuda.

--- nnfabrik/datasets/mouse.py
+++ nnfabrik/datasets/mouse.py
@@ -8,7 +8,7 @@ from mlutils.data.datasets import StaticImageSet
 from mlutils.data.transforms import Subsample, ToTensor
 
 
-def get_dataloader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, get_key=False, device='cuda'):
+def mouse_static_loader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, get_key=False, cuda=True):
     """
     returns a single data
     
@@ -21,7 +21,7 @@ def get_dataloader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tie
         tier (str, optional): tier is a placeholder to specify which set of images to pick for train, val, and test loader. Defaults to None.
         neuron_ids ([type], optional): select neurons by their ids. Defaults to None.
         get_key (bool, optional): whether to retun the data key, along with the dataloaders. Defaults to False.
-        device (str, optional): device to place the data on. Defaults to 'cuda'.
+        cuda (bool, optional): whether to place the data on gpu or not. Defaults to True.
 
     Returns:
         if get_key is False returns a dictionary of dataloaders for one dataset, where the keys are 'train', 'validation', and 'test'. 
@@ -39,19 +39,19 @@ def get_dataloader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tie
              (np.isin(dat.neurons.unit_ids, neuron_ids)))
     
     idx = np.where(conds)[0]
-    dat.transforms = [Subsample(idx), ToTensor(cuda=True if device=='cuda' else False)]
+    dat.transforms = [Subsample(idx), ToTensor(cuda)]
     
     # subsample images
     dataloaders = {}
     keys = [tier] if tier else ['train', 'validation', 'test']
     for tier in keys:
         
+        if img_seed is not None:
+            torch.manual_seed(img_seed)
+
         # sample images
         subset_idx = np.where(dat.tiers == tier)[0]
         sampler = SubsetRandomSampler(subset_idx)
-
-        if img_seed is not None:
-            torch.manual_seed(img_seed)
             
         dataloaders[tier] = DataLoader(dat, sampler=sampler, batch_size=batch_size)
     
@@ -60,7 +60,7 @@ def get_dataloader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tie
     return (data_key, dataloaders) if get_key else dataloaders
 
 
-def get_dataloaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, device='cuda'):
+def mouse_static_loaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, cuda=True):
     """
     Returns a dictionary of dataloaders (i.e., trainloaders, valloaders, and testloaders) for >= 1 dataset(s).
     
@@ -72,7 +72,7 @@ def get_dataloaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', t
         layer (str, optional): the layer from visual area. Defaults to 'L2/3'.
         tier (str, optional): tier is a placeholder to specify which set of images to pick for train, val, and test loader. Defaults to None.
         neuron_ids ([type], optional): select neurons by their ids. Defaults to None.
-        device (str, optional): device to place the data on. Defaults to 'cuda'.
+        cuda (bool, optional): whether to place the data on gpu or not. Defaults to True.
     
     Returns:
         dict: dictionary of dictionaries where the first level keys are 'train', 'validation', and 'test', and second level keys are data_keys.
@@ -84,9 +84,9 @@ def get_dataloaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', t
         dls[key] = OrderedDict({})
 
     for ind, path in enumerate(paths):
-        data_key, loaders = get_dataloader(path, batch_size, img_seed=img_seed, 
-                                           area=area, layer=layer, device=device,
-                                           tier=tier, get_key=True, neuron_ids=neuron_ids[ind] if neuron_ids else None)
+        data_key, loaders = mouse_static_loader(path, batch_size, img_seed=img_seed, 
+                                                area=area, layer=layer, cuda=cuda,
+                                                tier=tier, get_key=True, neuron_ids=neuron_ids[ind] if neuron_ids else None)
         for k in dls.keys():
             dls[k][data_key] = loaders[k]
             
