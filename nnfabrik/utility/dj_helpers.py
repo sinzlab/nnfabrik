fba0fb7500d2389081b044e874f9df2b651fea0e
Edgar Y. Walker <edgar.walker@gmail.com>
2020-01-21 07:49:45 -0600

    Fix hash generation to deal with nested object

--- nnfabrik/utility/dj_helpers.py
+++ nnfabrik/utility/dj_helpers.py
@@ -5,16 +5,46 @@ from datetime import datetime
 import hashlib
 import datajoint as dj
 from git import Repo, cmd
+from collections
 
-def make_hash(config):
-    """"
-    takes any non-nested dict to return a 32byte hash
-        :args: config -- dictionary. Must not contain objects or nested dicts
-        :returns: 32byte hash
+
+def make_hash(obj):
+    """
+    Given a Python object, returns a 32 character hash string to uniquely identify
+    the content of the object. The object can be arbitrary nested (i.e. dictionary 
+    of dictionary of list etc), and hashing is applied recursively to uniquely 
+    identify the content.
+    
+    For dictionaries (at any level), the key order is ignored when hashing
+    so that {"a":5, "b": 3, "c": 4} and {"b": 3, "a": 5, "c": 4} will both
+    give rise to the same hash. Exception to this rule is when an OrderedDict
+    is passed, in which case difference in key order is respected. To keep
+    compatible with previous versions of Python and the assumed general
+    intentions, key orderer will be ignored even in Python 3.7+ where the
+    default dictionary is officially an ordered dictionary.
+
+    Args: 
+        obj - A (potentially nested) Python object
+
+    Returns:
+        hash: str - a 32 charcter long hash string to uniquely identify the object.
     """
     hashed = hashlib.md5()
-    for k, v in sorted(config.items()):
-        hashed.update(str(v).encode())
+    
+    if isinstance(obj, OrderedDict):
+        for k, v in obj.items():
+            hashed.update(str(k).encode())
+            hashed.update(nested_hash(v).encode())
+    elif isinstance(obj, Mapping):
+        for k, v in sorted(obj.items()):
+            hashed.update(str(k).encode())
+            hashed.update(nested_hash(v).encode())
+    elif isinstance(obj, Iterable):
+        for v in obj:
+            hashed.update(nested_hash(v).encode())
+    else:
+        hashed.update(str(obj).encode())
+    
     return hashed.hexdigest()
 
 
