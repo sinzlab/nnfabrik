84622eaedcaae8be1a6c41e2a2a9d90b24904f71
Konstantin Willeke <konstantin.willeke@gmail.com>
2020-02-07 16:42:45 +0100

    Refactor V1 datasets

--- nnfabrik/datasets/__init__.py
+++ nnfabrik/datasets/__init__.py
@@ -1,4 +1,3 @@
-from . import csrf_v1_dataset, sysident_v1_dataset
-from .sysident_v1_dataset import sysident_v1
-from .csrf_v1_dataset import csrf_v1
+from . import sysident_v1_dataset
+from .sysident_v1_dataset import csrf_v1, sysident_v1
 from .toy_datasets import toy_dataset
