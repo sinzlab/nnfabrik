f1f844190993fc445c96d38e626a770b87c56aed
Konstantin Willeke <konstantin.willeke@gmail.com>
2020-03-10 22:40:24 +0100

    Update mouse dataloader function to work with new transforms as well as toy data. Needs the latest version of ml-utils, where the transforms were added to mlutils.data.transforms.

--- nnfabrik/datasets/mouse.py
+++ nnfabrik/datasets/mouse.py
@@ -1,18 +1,21 @@
 from collections import OrderedDict
 from itertools import zip_longest
 import numpy as np
+
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
 from mlutils.data.datasets import StaticImageSet
-from mlutils.data.transforms import Subsample, ToTensor
+from mlutils.data.transforms import Subsample, ToTensor, NeuroNormalizer, AddBehaviorAsChannels, SelectInputChannel
 from mlutils.data.samplers import SubsetSequentialSampler
 
 from ..utility.nn_helpers import set_random_seed
 
 
-def mouse_static_loader(path, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, get_key=False, cuda=True):
+def mouse_static_loader(path, batch_size, img_seed=None, area='V1', layer='L2/3',
+                        tier=None, neuron_ids=None, get_key=False, cuda=True, normalize=True, include_behavior=False,
+                        exclude=None, select_input_channel=None, toy_data=False, **kwargs):
     """
     returns a single data
     
@@ -33,17 +36,29 @@ def mouse_static_loader(path, batch_size, img_seed=None, area='V1', layer='L2/3'
 
     """
 
-    dat = StaticImageSet(path, 'images', 'responses')
-#     dat = StaticImageSet(path, 'inputs', 'targets')
+    dat = StaticImageSet(path, 'images', 'responses', 'behavior') if include_behavior else StaticImageSet(path, 'images', 'responses')
 
-    # specify condition(s) for sampling neurons. If you want to sample specific neurons define conditions that would effect idx
-    neuron_ids = neuron_ids if neuron_ids else dat.neurons.unit_ids
-    conds = ((dat.neurons.area == area) & 
-             (dat.neurons.layer == layer) &
-             (np.isin(dat.neurons.unit_ids, neuron_ids)))
-    
-    idx = np.where(conds)[0]
-    dat.transforms = [Subsample(idx), ToTensor(cuda)]
+    assert (include_behavior and select_input_channel) is False, "Selecting an Input Channel and Adding Behavior can not both be true"
+
+    if toy_data:
+        dat.transforms = [ToTensor(cuda)]
+    else:
+        # specify condition(s) for sampling neurons. If you want to sample specific neurons define conditions that would effect idx
+        neuron_ids = neuron_ids if neuron_ids else dat.neurons.unit_ids
+        conds = ((dat.neurons.area == area) &
+                 (dat.neurons.layer == layer) &
+                 (np.isin(dat.neurons.unit_ids, neuron_ids)))
+
+        idx = np.where(conds)[0]
+        dat.transforms = [Subsample(idx), ToTensor(cuda)]
+        if normalize:
+            dat.transforms.insert(1, NeuroNormalizer(dat, exclude=exclude))
+
+        if include_behavior:
+            dat.transforms.insert(0, AddBehaviorAsChannels())
+
+        if select_input_channel is not None:
+            dat.transforms.insert(0, SelectInputChannel(select_input_channel))
     
     # subsample images
     dataloaders = {}
@@ -62,11 +77,15 @@ def mouse_static_loader(path, batch_size, img_seed=None, area='V1', layer='L2/3'
     
     # create the data_key for a specific data path 
     data_key = path.split('static')[-1].split('.')[0].replace('preproc', '')
-    
+    if toy_data:
+        data_key = path.split('toy_')[-1][:13]
+
     return (data_key, dataloaders) if get_key else dataloaders
 
 
-def mouse_static_loaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None, neuron_ids=None, cuda=True):
+def mouse_static_loaders(paths, batch_size, img_seed=None, area='V1', layer='L2/3', tier=None,
+                         neuron_ids=None, cuda=True, normalize=False, include_behavior=False,
+                         exclude=None, select_input_channel=None, toy_data=False, **kwargs):
     """
     Returns a dictionary of dataloaders (i.e., trainloaders, valloaders, and testloaders) for >= 1 dataset(s).
     
@@ -82,8 +101,8 @@ def mouse_static_loaders(paths, batch_size, img_seed=None, area='V1', layer='L2/
     
     Returns:
         dict: dictionary of dictionaries where the first level keys are 'train', 'validation', and 'test', and second level keys are data_keys.
-    """ 
-    
+    """
+
     neuron_ids = neuron_ids if neuron_ids is not None else []
 
     dls = OrderedDict({})
@@ -92,10 +111,13 @@ def mouse_static_loaders(paths, batch_size, img_seed=None, area='V1', layer='L2/
         dls[key] = OrderedDict({})
 
     for path, neuron_id in zip_longest(paths, neuron_ids, fillvalue=None):
-        data_key, loaders = mouse_static_loader(path, batch_size, img_seed=img_seed, 
+        data_key, loaders = mouse_static_loader(path, batch_size, img_seed=img_seed,
                                                 area=area, layer=layer, cuda=cuda,
-                                                tier=tier, get_key=True, neuron_ids=neuron_id)
+                                                tier=tier, get_key=True, neuron_ids=neuron_id,
+                                                normalize=normalize, include_behavior=include_behavior,
+                                                exclude=exclude, select_input_channel=select_input_channel,
+                                                toy_data=toy_data)
         for k in dls:
             dls[k][data_key] = loaders[k]
-            
+
     return dls
