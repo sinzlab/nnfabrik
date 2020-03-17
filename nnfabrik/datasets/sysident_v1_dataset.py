47f6ebf4efa2fd0d70b5c53d7ff322ab8d4d49fe
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-03-17 23:08:22 +0100

    Add Cached Version of the CSRF dataloader function.

--- nnfabrik/datasets/sysident_v1_dataset.py
+++ nnfabrik/datasets/sysident_v1_dataset.py
@@ -4,6 +4,206 @@ import numpy as np
 import pickle
 #from retina.retina import warp_image
 from collections import namedtuple, Iterable
+import os
+
+class ImageCache:
+    """
+    A simple cache which loads images into memory given a path to the directory where the images are stored.
+    Images need to be present as 2D .npy arrays
+    """
+    def __init__(self, path=None, subsample=1, crop=0, img_mean=None, img_std=None, leading_zeros=6):
+        """
+
+        :param path: str - pointing to the directory, where the individual .npy files are present
+        :param subsample: int - amount of downsampling
+        :param crop: int - crops the specified amount of pixles symmetrically from all sides
+        :param img_mean: - mean luminance across all images
+        :param img_std: - std of the luminance across all images
+        :param leading_zeros: - amount leading zeros of the files in the specified folder
+        """
+        self.cache = {}
+        self.path = path
+        self.subsample = subsample
+        self.crop = crop
+        self.img_mean = img_mean
+        self.img_std = img_std
+        self.leading_zeros = leading_zeros
+
+    def __contains__(self, key):
+        return key in self.cache
+
+    def update(self, key):
+        filename = self.path + str(key).zfill(self.leading_zeros) + '.npy'
+        image = np.load(filename)
+        transformed_image = self._transform_image(image)
+        self.cache[key] = transformed_image
+
+    def _transform_image(self, image):
+        """
+        applies transformations to the image: downsampling and cropping, z-scoring, and dimension expansion.
+        """
+        h, w = image.shape
+        image = image[self.crop:h - self.crop:self.subsample, self.crop:w - self.crop:self.subsample]
+        image = (image - self.img_mean) / self.img_std
+        image = image[None, ]
+        return torch.tensor(image).to(torch.float)
+
+    @property
+    def size(self):
+        return len(self.cache)
+
+
+class CachedTensorDataset(utils.Dataset):
+    """
+    Dataset wrapping tensors.
+
+    Each sample will be retrieved by indexing tensors along the first dimension.
+
+    Arguments:
+        *tensors (Tensor): tensors that have the same size of the first dimension.
+    """
+    def __init__(self, *tensors, names=('inputs','targets'), image_cache=None):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        assert len(tensors) == len(names)
+        self.tensors = tensors
+        self.DataPoint = namedtuple('DataPoint', names)
+        self.image_cache = image_cache
+
+    def __getitem__(self, index):
+        """
+        retrieves the inputs (= tensors[0]) from the image cache. If the image ID is not present in the cache,
+            the cache is updated to load the corresponding image into memory.
+        """
+        key = self.tensors[0][index].item()
+        if key not in self.image_cache:
+            self.image_cache.update(key)
+        return self.DataPoint(*[self.image_cache.cache[key], *[tensor[index] for tensor in self.tensors[1:]]])
+
+    def __len__(self):
+        return self.tensors[0].size(0)
+
+
+def get_cached_loader(image_ids, responses, batch_size, shuffle=True, image_cache=None):
+    """
+    Args:
+        images: Numpy Array of Images, Dimensions: N x C x W x H
+        responses: Numpy Array, Dimensions: N_images x Neurons
+        batch_size: int - batch size for the dataloader
+        shuffle: Boolean, shuffles image in the dataloader if True
+
+    Returns: a PyTorch DataLoader object
+
+    """
+
+    # Expected Dimension of the Image Tensor is Images x Channels x size_x x size_y
+    # In some CSRF files, Channels are at Dim4, the image tensor is thus reshaped accordingly
+    image_ids = torch.tensor(image_ids)
+    responses = torch.tensor(responses).to(torch.float)
+
+    dataset = NamedTensorDataset(image_ids, responses, image_cache=image_cache)
+    data_loader = utils.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+    return data_loader
+
+
+def monkey_csrf(datafiles, imagepath, batch_size, seed,
+            train_frac=0.8, subsample=1, crop=65,
+            time_bins_sum=tuple(range(12)), avg=False,
+            crop_h=None, crop_w=None):
+    """
+    Function that returns cached dataloaders for the Center Surround Visual Field Experiments.
+        Data recorded by George and Kelli at BCM, Houston.
+
+     creates a nested dictionary of dataloaders in the format
+            {'train' : dict_of_loaders,
+             'validation'   : dict_of_loaders,
+            'test'  : dict_of_loaders, }
+
+        in each dict_of_loaders, there will be  one dataloader per data-key (refers to a unique session ID)
+        with the format:
+            {'data-key1': torch.utils.data.DataLoader,
+             'data-key2': torch.utils.data.DataLoader, ... }
+
+    required inputs is a list of datafiles specified as a full path, together with a full path
+        to a file that contains all the actually images
+
+    Args:
+        datafiles: a list paths that point to pickle files
+        imagepath: a path that points to the image files
+        batch_size: int - batch size of the dataloaders
+        seed: int - random seed, to calculate the random split
+        train_frac: ratio of train/validation images
+        subsample: int - downsampling factor
+        crop: int - crops x pixels from each side. Example: Input image of 100x100, crop=10 => Resulting img = 80x80
+        time_bins_sum: sums the responses over x time bins.
+        avg: Boolean - Sums oder Averages the responses across bins.
+
+    Returns: nested dictionary of dataloaders
+    """
+
+    # initialize dataloaders as empty dict
+    dataloaders = {'train': {}, 'validation': {}, 'test': {}}
+
+    if not isinstance(time_bins_sum, Iterable):
+        time_bins_sum = tuple(range(time_bins_sum))
+
+    if imagepath:
+        with open(imagepath, "rb") as pkl:
+            images = pickle.load(pkl)
+
+    images = images[:, :, :, None]
+    _, h, w = images.shape[:3]
+    if crop_h is None and crop_w is None:
+        images_cropped = images[:, crop:h - crop:subsample, crop:w - crop:subsample, :]
+    else:
+        images_cropped = images[:, crop_h[0]:h - crop_h[1]:subsample, crop_w[0]:w - crop_w[1]:subsample, :]
+    img_mean = np.mean(images_cropped)
+    img_std = np.std(images_cropped)
+
+    all_train_ids, all_validation_ids = get_validation_split(n_images=images.shape[0],
+                                                             train_frac=train_frac,
+                                                             seed=seed)
+
+    # Initialize the Image Cache class
+    path = os.path.join(os.path.split(imagepath)[0], 'image_arrays')
+    Cache = ImageCache(path=path, subsample=subsample, crop=crop, img_mean=img_mean, img_std=img_std)
+
+    # cycling through all datafiles to fill the dataloaders with an entry per session
+    for i, datapath in enumerate(datafiles):
+
+        with open(datapath, "rb") as pkl:
+            raw_data = pickle.load(pkl)
+
+        subject_ids = raw_data["subject_id"]
+        data_key = str(raw_data["session_id"])
+        repetitions_test = raw_data["testing_repetitions"]
+        responses_train = raw_data["training_responses"].astype(np.float32)
+        responses_test = raw_data["testing_responses"].astype(np.float32)
+        training_image_ids = raw_data["training_image_ids"]
+        testing_image_ids = raw_data["testing_image_ids"]
+        responses_test = responses_test.transpose((2, 0, 1))
+        responses_train = responses_train.transpose((2, 0, 1))
+
+        if time_bins_sum is not None:  # then average over given time bins
+            responses_train = (np.mean if avg else np.sum)(responses_train[:, :, time_bins_sum], axis=-1)
+            responses_test = (np.mean if avg else np.sum)(responses_test[:, :, time_bins_sum], axis=-1)
+
+        train_idx = np.isin(training_image_ids, all_train_ids)
+        val_idx = np.isin(training_image_ids, all_validation_ids)
+
+        responses_val = responses_train[val_idx]
+        responses_train = responses_train[train_idx]
+
+        train_loader = get_cached_loader(np.array(train_idx, dtype=np.int16), responses_train, batch_size=batch_size, image_cache=Cache)
+        val_loader = get_cached_loader(np.array(val_idx, dtype=np.int16), responses_val, batch_size=batch_size, image_cache=Cache)
+        test_loader = get_cached_loader(np.array(testing_image_ids, dtype=np.int16), responses_test, batch_size=batch_size, shuffle=False, image_cache=Cache)
+
+        dataloaders["train"][data_key] = train_loader
+        dataloaders["validation"][data_key] = val_loader
+        dataloaders["test"][data_key] = test_loader
+
+    return dataloaders
+
 
 
 class NamedTensorDataset(utils.Dataset):
@@ -15,8 +215,7 @@ class NamedTensorDataset(utils.Dataset):
     Arguments:
         *tensors (Tensor): tensors that have the same size of the first dimension.
     """
-
-    def __init__(self, *tensors, names=('inputs','targets')):
+    def __init__(self, *tensors, names=('inputs','targets'), image_cache=None):
         assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
         assert len(tensors) == len(names)
         self.tensors = tensors
