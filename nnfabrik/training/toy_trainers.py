70dd57e1f61f8557bb75dc7162f9ea6655e07a0c
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2020-04-22 13:15:16 +0200

    Fix toy examples.

--- nnfabrik/training/toy_trainers.py
+++ nnfabrik/training/toy_trainers.py
@@ -1,9 +1,9 @@
-def toy_trainer(model, seed, train, validation, test, param1=0):
+def toy_trainer(model, dataloaders, seed, param1=0):
     """
     a toy training function that with toy trained model outputs
     """
-    some_loss = 1
+    a_score = 1
     some_output = 2
     some_model_state = 3
     
-    return some_loss, some_output, some_model_state
+    return a_score, some_output, some_model_state
