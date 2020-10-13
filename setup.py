7ab03089dcedd26104714ed15522f77cffae5ad2
Edgar Y. Walker <edgar.walker@gmail.com>
2020-10-13 17:42:58 +0200

    Increment to 0.0.1rc0

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="nnfabrik",
-    version="0.0.1",
-    description="Factory for Neural Networks",
+    version="0.0.1rc0",
+    description="A generalized model fitting pipeline",
     author="Sinz Lab",
     author_email="software@sinzlab.net",
     packages=find_packages(exclude=[]),
