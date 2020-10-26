0bb8e8b05db286b003401f60105c0cd9749d0072
Edgar Y. Walker <edgar.walker@gmail.com>
2020-10-26 10:29:08 +0000

    Update and add more doc to set_random_seed

--- nnfabrik/utility/nn_helpers.py
+++ nnfabrik/utility/nn_helpers.py
@@ -2,7 +2,6 @@
 
 import torch
 from torch import nn
-from torch.backends import cudnn
 
 from neuralpredictors.training import eval_state
 import numpy as np
@@ -61,19 +60,28 @@ def get_module_output(model, input_shape):
 
 def set_random_seed(seed: int, deterministic: bool = True):
     """
-    Sets all random seeds
+    Set random generator seed for Python interpreter, NumPy and PyTorch. When setting the seed for PyTorch,
+    if CUDA device is available, manual seed for CUDA will also be set. Finally, if `deterministic=True`,
+    and CUDA device is available, PyTorch CUDNN backend will be configured to `benchmark=False` and `deterministic=True`
+    to yield as deterministic result as possible. For more details, refer to 
+    PyTorch documentation on reproducibility: https://pytorch.org/docs/stable/notes/randomness.html
 
-    :param seed: (int) seed to be set
-    :param deterministic: (bool) activates cudnn.deterministic, which might slow down things
+    Beware that the seed setting is a "best effort" towards deterministic run. However, as detailed in the above documentation,
+    there are certain PyTorch CUDA opertaions that are inherently non-deterministic, and there is no simple way to control for them.
+    Thus, it is best to assume that when CUDA is utilized, operation of the PyTorch module will not be deterministic and thus
+    not completely reproducible.
+
+    Args:
+        seed (int): seed value to be set
+        deterministic (bool, optional): If True, CUDNN backend (if available) is set to be deterministic. Defaults to True. Note that if set
+            to False, the CUDNN properties remain untouched and it NOT explicitly set to False.
     """
-    np.random.seed(seed)
     random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        if deterministic:
-            cudnn.benchmark = False
-            cudnn.deterministic = True
-        torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    if deterministic:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)  # this sets both CPU and CUDA seeds for PyTorch
 
 
 def move_to_device(model, gpu=True, multi_gpu=True):
