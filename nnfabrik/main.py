44e46f8d6b0eec52aa0df561bab492cf3f6c45c4
Edgar Y. Walker <edgar.walker@gmail.com>
2020-10-27 19:36:54 +0000

    Allow context to be specified for custom_nnfabrik

--- nnfabrik/main.py
+++ nnfabrik/main.py
@@ -354,7 +354,11 @@ class Seed(dj.Manual):
 
 
 def custom_nnfabrik(
-    schema, use_common_fabrikant=True, module_name=None, spawn_existing_tables=False
+    schema,
+    use_common_fabrikant=True,
+    module_name=None,
+    context=None,
+    spawn_existing_tables=False,
 ):
     """
     Create a custom nnfabrik module under specified DataJoint schema,
@@ -369,43 +373,48 @@ def custom_nnfabrik(
            Defaults to True.
         module_name (str, optional): Name property of the returned Python module object.
             Defaults to None, in which case the name of the schema will be used.
+        context (dict, optional): If non None value is provided, then a module is not created and
+            instead the tables are defined inside the context.
         spawn_existing_tables (bool, optional): If True, perform `spawn_missing_tables` operation
             onto the newly created table. Defaults to False.
 
     Returns:
-        Python Module object: A new Python module containing nnfabrik tables defined under
-            the schema. The module's schema property points to the schema object as well.
+        Python Module object or None: If `context` was None, a new Python module containing 
+            nnfabrik tables defined under the schema. The module's schema property points 
+            to the schema object as well. Otherwise, nothing is returned.
     """
     if isinstance(schema, str):
         schema = CustomSchema(schema)
 
     tables = [Fabrikant, Model, Dataset, Trainer]
 
-    module_name = schema.database if module_name is None else module_name
-    module = types.ModuleType(module_name)
-    setattr(module, "schema", schema)
+    module = None
+    if context is None:
+        module_name = schema.database if module_name is None else module_name
+        module = types.ModuleType(module_name)
+        context = module.__dict__
+
+    context["schema"] = schema
 
     # spawn all existing tables into the module
     # TODO: replace with a cheaper check operation
-    if spawn_existing_tables:
-        context = module.__dict__
-    else:
-        context = {}
-
-    schema.spawn_missing_classes(context)
+    temp_context = context if spawn_existing_tables else {}
+    schema.spawn_missing_classes(temp_context)
 
     if use_common_fabrikant:
-        if "Fabrikant" in context:
+        if "Fabrikant" in temp_context:
             warnings.warn(
                 "The schema already contained Fabrikant table. Using that table instead of the common one!"
             )
         else:
-            setattr(module, "Fabrikant", Fabrikant)
+            context["Fabrikant"] = Fabrikant
             # skip creating Fabrikant table
             tables.pop(0)
 
     for table in tables:
         new_table = type(table.__name__, (table,), dict(__doc__=table.__doc__))
-        setattr(module, table.__name__, schema(new_table, context=module.__dict__))
+        context[table.__name__] = schema(new_table, context=context)
 
+    # this return None if context was set
     return module
+
